eaf2f835cd534fa286cdf0031e3ace41a389265b
Leonardo Covarrubias <leo@leocov.com>
2024-06-18 20:33:43 -0400

    feat: use protoc-bin-vendored crate

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 use prost_build;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path = &protoc_bin_vendored::protoc_bin_path().unwrap();
+    std::env::set_var("PROTOC", protoc_path);
+
     let mut config = prost_build::Config::new();
     config.protoc_arg("--experimental_allow_proto3_optional");
 
